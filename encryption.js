f939bcc50f4955c8421cd343fcb77f8a9190f433
suman kumar <146518730+sumansingh20@users.noreply.github.com>
2025-08-26 22:59:53 +0530

    Remove empty line in encryption.js
    Remove unnecessary empty line at the beginning of the file.

--- encryption.js
+++ encryption.js
@@ -2,7 +2,6 @@
 const crypto = require('crypto');
 const NodeRSA = require('node-rsa');
 
-
 // Define the EncryptionService class for handling AES and RSA encryption
 class EncryptionService {
     constructor() {
