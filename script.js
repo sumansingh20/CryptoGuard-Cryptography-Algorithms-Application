ebaaeae7d12449bce6220d11a19377fc8d8c5ddf
suman kumar <146518730+sumansingh20@users.noreply.github.com>
2025-09-22 23:57:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
 
 
 
+
 // Encryption form submission handler
 document.getElementById('encryptionForm').addEventListener('submit', async (event) => {
     event.preventDefault(); // Prevents the form from submitting in the default way
